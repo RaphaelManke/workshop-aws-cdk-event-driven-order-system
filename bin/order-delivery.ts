b53d510b786ba99a5f7148edf2dc55b421d4f042
Raphael Manke <raphael.manke@codecentric.de>
2022-10-08 19:27:46 +0200

    fix: import

--- bin/order-delivery.ts
+++ bin/order-delivery.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
+import { App } from "aws-cdk-lib";
 import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
 import { OrderDeliveryStack } from "../lib/order-delivery-stack";
 
 /**
  * Replace this email with your email
  */
 const orderEmail = process.env.ORDER_EMAIL || "";
-const app = new cdk.App();
+const app = new App();
 new OrderDeliveryStack(app, "OrderDeliveryStack", {
   orderEmail,
 });
